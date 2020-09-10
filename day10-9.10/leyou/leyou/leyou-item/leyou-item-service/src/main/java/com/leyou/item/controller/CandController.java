c405940b8f881dc77fe579161aaae5af05924d00
yuan <2173378960@qq.com>
2020-09-10 21:06:58 +0800

    提交day10作业

---
+++ day10-9.10/leyou/leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CandController.java
@@ -0,0 +1,28 @@
+package com.leyou.item.controller;
+
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class CandController {
+
+    @Autowired
+    BrandService brandService;
+
+    @PostMapping
+    public ResponseEntity<Void> saveBrand(Brand brand, @RequestParam("cids")List<Long> cids){
+        this.brandService.saveBrand(brand,cids);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
+
+}
